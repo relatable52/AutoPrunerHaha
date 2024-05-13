c9f695638fb8d20794e8253d563bb33adc1452aa
Chinh Tran <93480420+relatable52@users.noreply.github.com>
2024-05-13 16:58:37 +0700

    Update utils.py

--- src/finetune/utils.py
+++ src/finetune/utils.py
@@ -6,20 +6,23 @@ from torch.utils.data import DataLoader
 from src.finetune.model import BERT
 import numpy as np
 from torch import nn
+import argparse
+from src.utils.utils import Logger, AverageMeter, evaluation_metrics, read_config_file
+from src.finetune.main import get_model
 
 PARAMS = {'batch_size': 10, 'shuffle': False, 'num_workers': 8} 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-def save_finetune(config, mode):
+def save_finetune(config, mode, model_name):
     dataset= CallGraphDataset(config, mode)
     dataloader = DataLoader(dataset, **PARAMS)
-    model_path = os.path.join(config["LEARNED_MODEL_DIR"], "model.pth")
-    save_dir = os.path.join(config["CACHE_DIR"], "{}_finetuned".format(mode))
+    model_path = os.path.join(config["LEARNED_MODEL_DIR"], f"{model_name}/", "model.pth")
+    save_dir = os.path.join(config["CACHE_DIR"], f"{model_name}/", "{}_finetuned".format(mode))
     
     if not os.path.exists(save_dir):
-        os.mkdir(save_dir)
+        os.makedirs(save_dir)
     
-    model=BERT()
+    model = get_model(model_name=model_name)
 
     if torch.cuda.device_count() > 1:
         print("Let's use", torch.cuda.device_count(), "GPUs!")
@@ -36,6 +39,20 @@ def save_finetune(config, mode):
         emb = emb.detach().cpu().numpy()
         save_path = os.path.join(save_dir, "{}.npy".format(idx))
         np.save(save_path, emb)
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config_path", type=str, default="config/wala.config") 
+    parser.add_argument("--model_name", type=str, default="codet5p-110m-embedding")
+    
+    return parser.parse_args()
+
+def main():
+    args = get_args()
+    config = read_config_file(args.config_path)
+    mode = args.mode
+    model_name = args.model_name
+    save_finetune(config, mode, model_name)
     
 
 
